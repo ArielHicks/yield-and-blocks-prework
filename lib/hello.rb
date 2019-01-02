b24936794d9a2765a226de43464c4c1e9a68e170
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-02 15:00:42 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,39 +1,5 @@
-#Ruby Blocks
-10.times do
-  p "my name"
-end
-
-#the example above is a built in Ruby block
-# Now, we will build our own block
-
-def times number
-  number * 2
-end
-
-times 2 do
+def hello_t
 
 end
 
-puts"========="
-
-#what if we wanted to make our block more dynamic?
-def times number
-  number * yield
-end
-
-times 2 do
-  4       #THIS ends up being the yield value in the def blocke above
-end
-
-puts"==============="
-
-def times number
-  number * yield
-end
-
-times 2 do
-  a = 4
-  b = 8
-  c = a + b     #REMEMBER that Ruby only returns the last line in the block, c = the yield
-end
-#our answer is 24
+# call your method here!
